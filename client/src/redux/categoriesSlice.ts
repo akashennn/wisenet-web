8f918d7c9fa3b52438ef80fb1333e25a459fb817
Akash Perera <akash.perera@chope.co>
2023-02-10 14:17:58 +0530

    add getCategory

--- client/src/redux/categoriesSlice.ts
+++ client/src/redux/categoriesSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { TSidebarCategory } from '../types';
+import { TCategory, TSidebarCategories } from '../types';
 
 const BASE_ENDPOINT = "/graphql";
 
@@ -8,13 +8,23 @@ const COMMON_HEADERS = {
 };
 
 interface InitialState {
-    isLoading: boolean;
-    sidebarCategories: TSidebarCategory[];
+    isSidebarCategoriesLoading: boolean;
+    sidebarCategories: TSidebarCategories[];
+
+    activeCategoryId: number;
+
+    isCategoryLoading: boolean;
+    category: TCategory | null;
 }
 
 const initialState: InitialState = {
-    isLoading: false,
+    isSidebarCategoriesLoading: false,
     sidebarCategories: [],
+
+    activeCategoryId: 1,
+
+    isCategoryLoading: false,
+    category: null,
 }
 
 export const getAllSidebarCategories = createAsyncThunk('categories/getAllSidebarCategories', async () => {
@@ -39,6 +49,40 @@ export const getAllSidebarCategories = createAsyncThunk('categories/getAllSideba
     return data.data.categories;
 });
 
+export const getCategory = createAsyncThunk('categories/getCategory', async (activeCategoryId: number) => {
+    const graphqlQuery = {
+        "operationName": "getCategory",
+        "query": `query getCategory {
+            category(id:${activeCategoryId}) {
+              id
+              name
+              articleCount
+              articles {
+                id
+                name
+                variantName
+                prices {
+                  currency
+                  value
+                }
+                images {
+                  path
+                }
+              }
+            }
+          }`,
+        "variables": {}
+    };
+
+    const response = await fetch(BASE_ENDPOINT, {
+        "method": "POST",
+        "headers": COMMON_HEADERS,
+        "body": JSON.stringify(graphqlQuery)
+    });
+    const data = await response.json();
+    return data.data.category;
+});
+
 export const categoriesSlice = createSlice({
     name: 'categories',
     initialState,
@@ -49,16 +93,34 @@ export const categoriesSlice = createSlice({
     },
     extraReducers: builder => {
         builder
+            // getAllSidebarCategories
             .addCase(getAllSidebarCategories.pending, (state) => {
-                state.isLoading = true;
+                state.isSidebarCategoriesLoading = true;
             })
             .addCase(getAllSidebarCategories.fulfilled, (state, { payload }) => {
-                state.isLoading = false;
+                state.isSidebarCategoriesLoading = false;
                 state.sidebarCategories = payload;
             })
             .addCase(getAllSidebarCategories.rejected, (state, { payload }) => {
-                state.isLoading = false;
+                state.isSidebarCategoriesLoading = false;
                 console.error('getAllSidebarCategories.rejected', payload);
+            })
+
+            // getCategory
+            .addCase(getCategory.pending, (state) => {
+                state.isCategoryLoading = true;
+            })
+            .addCase(getCategory.fulfilled, (state, { payload }) => {
+                console.log('payload', payload)
+                state.isCategoryLoading = false;
+                state.category = payload;
+
+                // graphQL returns null when categoryId is invalid
+                state.activeCategoryId = payload?.id;
+            })
+            .addCase(getCategory.rejected, (state, { payload }) => {
+                state.isCategoryLoading = false;
+                console.error('getCategory.rejected', payload);
             })
     }
 });
