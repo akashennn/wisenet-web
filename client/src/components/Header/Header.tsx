01180af6192568c71653f7385cc4aece776656cc
Akash Perera <akash.perera@chope.co>
2023-02-10 18:37:42 +0530

    fix mobile responsiveness

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,22 +1,58 @@
 import React from "react";
 import styled from "styled-components";
+import { setSearchText } from "../../redux/categoriesSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import Input from "../Input/Input";
 
 const Header = (): JSX.Element => {
+  // get data from redux
+  const { searchText } = useAppSelector((state) => state.categories);
+
+  // redux hooks to call functions
+  const dispatch = useAppDispatch();
+
   return (
     <Container>
-      <strong>SHINE INT</strong>
+      <p className="title">SHINE INT</p>
 
-      <input placeholder="Search" />
+      <Input
+        placeholder="Search category..."
+        value={searchText}
+        onChange={(e) => dispatch(setSearchText(e.target.value))}
+      />
     </Container>
   );
 };
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   grid-area: header;
+  box-sizing: border-box;
   background-color: lightblue;
+  padding: 24px 12px;
 
-  input {
-    float: right;
+  .title {
+    font-weight: bold;
+    color: rgb(61, 61, 61);
+    font-size: 1.25rem;
+    margin: 0 0 24px 0;
+  }
+
+  /* styles for tablets */
+  @media (min-width: 768px) {
+  }
+
+  /* styles for desktops */
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 60px;
+
+    .title {
+      margin: 0;
+    }
   }
 `;
 
