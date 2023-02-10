de59f58b715ada32e67792a243d743391ee47e19
Akash Perera <akash.perera@chope.co>
2023-02-10 19:48:47 +0530

    add styles

--- client/src/pages/Error/Error.tsx
+++ client/src/pages/Error/Error.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const ErrorPage = (): JSX.Element => {
   return (
-    <div>
-      <h2>404</h2>
-
-      <p>page not found</p>
-
-      <Link to="/">back home</Link>
-    </div>
+    <Container>
+      <p className="title">404</p>
+      <p className="error">Page not found!</p>
+      <Link to="/" className="button">
+        Visit Home Page
+      </Link>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  p {
+    margin: 0;
+  }
+
+  .title {
+    font-size: 4rem;
+  }
+
+  .error {
+    font-size: 1.25rem;
+    margin-bottom: 12px;
+  }
+
+  .button {
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+  }
+`;
 
 export default ErrorPage;
